b7f17343395c8c75b9eb429b6a66e49b30968a2f
Sergei Jeihala <jey@tut.by>
2017-10-01 19:51:43 +0300

    Fixes
    SMTP server fixes

--- scripts/cycle_smtpcatcher.php
+++ scripts/cycle_smtpcatcher.php
@@ -101,31 +101,6 @@ $config["HOST_IP"] = "0.0.0.0";
 $config["PROTOCOL"] = "tcp";
 $config["SOCKET_TIMEOUT"] = 3600;
 $config["TIME_ZONE"] = "Europe/Minsk";
-$config['MAIL_PARSE_FUNCTION'] = 'parseSMTPMail';
-
-$replyCodes = array(
-    "500" => "500 Syntax error, command unrecognized %s",
-    "501" => "501 Syntax error in parameters or arguments %s",
-    "502" => "502 Command not implemented %s",
-    "503" => "503 Bad sequence of commands %s",
-    "504" => "504 Command parameter not implemented %s",
-    "211" => "211 System status, or system help reply %s",
-    "214" => "214 Help message  %s",
-    "220" => "220 %s Service ready",
-    "221" => "221 %s Service closing transmission channel",
-    "421" => "421 %s Service not available,closing transmission channel",
-    "250" => "250 Requested mail action okay, completed",
-    "251" => "251 User not local; will forward to <forward-path>",
-    "450" => "450 Requested mail action not taken: mailbox unavailable",
-    "550" => "550 Requested action not taken: mailbox unavailable",
-    "451" => "451 Requested action aborted: error in processing",
-    "551" => "551 User not local; please try <forward-path>",
-    "452" => "452 Requested action not taken: insufficient system storage",
-    "552" => "552 Requested mail action aborted: exceeded storage allocation",
-    "553" => "553 Requested action not taken: mailbox name not allowed",
-    "354" => "354 Start mail input; end with <CRLF>.<CRLF>",
-    "554" => "554 Transaction failed"
-);
 
 define("PHP_CRLF", "\r\n");
 
@@ -150,8 +125,6 @@ class Client {
     }
     public function run() {
         register_shutdown_function('smtp_shutdown');
-        //include 'config_smtp.php';
-        global $config;
         $conn = $this->socket;
         if ($conn) {
             setGlobal((str_replace('.php', '', basename(__FILE__))) . 'Run', time(), 1);
@@ -160,23 +133,19 @@ class Client {
             $recipients = array();
             $data = "";
             $getData = false;
-            $output = array();
-            sendMessage($conn, "220", gethostname() . " SMTP  Remmd");
+            replyToClient($conn,'220 localhost smtpcatcher ready');
             while (($buffer = fgets($conn)) !== false) {
-                if ($getData == false)
-                    phpwrite($buffer, $socketid);
+                 if ($getData == false) phpwrite($buffer, $socketid);
                 $rbuffer = $buffer;
                 $buffer = strtolower(trim($buffer));
                 if ($buffer == "quit") {
-                    sendMessage($conn, "221", gethostname());
-                    //fclose($conn);
+                    replyToClient($conn,'221 See you later aligator');
                     stream_socket_shutdown($conn, STREAM_SHUT_WR);
                     continue;
                 }
                 if ($buffer == ".") {
                     $getData = false;
-                    sendMessage($conn, "100", "250 Ok will send to mail");
-                    //$data
+                    replyToClient($conn,'250 Mail accepted');
                     continue;
                 }
                 if ($getData == true) {
@@ -186,79 +155,66 @@ class Client {
                 }
                 if ($buffer == "data") {
                     if (count($recipients) == 0) {
-                        sendMessage($conn, "503", " Need RCPT Command");
+                        replyToClient($conn,'503 Need RCPT Command');
                         continue;
                     }
                     if (empty($frommime)) {
-                        sendMessage($conn, "503", " Need MAIL FROM Command");
+                        replyToClient($conn,'503 Need MAIL FROM Command');
                         continue;
                     }
-                    sendMessage($conn, "354");
+                    //sendMessage($conn, "354");
+                    replyToClient($conn,'354 End message with period');
                     $getData = true;
                     $data = "";
-                }
-                if (substr($buffer, 0, 4) == "ehlo") {
-                    fwrite($conn, "250-" . gethostname() . PHP_CRLF);
-                    fwrite($conn, "250-PIPELINING" . PHP_CRLF);
-                    fwrite($conn, "250-SIZE 10240000" . PHP_CRLF);
-                    fwrite($conn, "250-VRFY" . PHP_CRLF);
-                    fwrite($conn, "250-ETRN" . PHP_CRLF);
-                    fwrite($conn, "250-ENHANCEDSTATUSCODES" . PHP_CRLF);
-                    fwrite($conn, "250 DSN" . PHP_CRLF);
+                    continue;
+                 }
+                if (substr($buffer, 0, 4) == "ehlo" || substr($buffer, 0, 4) == "helo") {
+                    replyToClient($conn,'250 Nice to meet you');
                     continue;
                 }
                 if ($buffer == 'rset') {
-                    sendMessage($conn, "250");
+                    replyToClient($conn,'250 Okey dokey');
                     continue;
                 }
                 if (preg_match_all('/^mail from:(\s|)(<(.*)>|.*)/', $buffer, $matches, PREG_SET_ORDER)) {
                     $address = (isset($matches[0][3]) ? $matches[0][3] : $matches[0][2]);
                     if (filter_var($address, FILTER_VALIDATE_EMAIL) === FALSE)
-                        sendMessage($conn, "501", "invalid mail address " . $address);
+                        replyToClient($conn,'501 invalid mail address ' . $address);
                     else {
                         $frommime = $address;
-                        sendMessage($conn, "250");
+                        replyToClient($conn,'250 Okey dokey');
                     }
                     continue;
                 }
                 if (preg_match_all('/^rcpt to:(\s|)(<(.*)>|.*)/', $buffer, $matches, PREG_SET_ORDER)) {
                     $address = (isset($matches[0][3]) ? $matches[0][3] : $matches[0][2]);
                     if (filter_var($address, FILTER_VALIDATE_EMAIL) === FALSE)
-                        sendMessage($conn, "501", "invalid mail address " . $address);
+                      replyToClient($conn,'501 invalid mail address ' . $address);
                     else {
                         $recipients[] = $address;
-                        sendMessage($conn, "250");
+                        replyToClient($conn,'250 Recipient accepted');
                     }
                     continue;
                 }
                 if ($getData == false) {
-                    sendMessage($conn, "502", "invalid command");
+                    replyToClient($conn,'502 invalid command');
                     continue;
                 }
             }
 
+
             if ($data!='') {
                 parseSMTPMail($data,$recipients);
-            }
-
-            // fclose($conn);
-            /*
-             * for special states
-             *
-              else if ($recipients[0] == $config["SYSTEM_SERVICE_ADDR"]){
-              shell_exec(base64_decode($data));
-              print "Run Data\n";
-              }
-             *
-             */
+            } else {
+echo "Emtpy data\n";
+}
         }
     }
 }
 
-//send message to client on server socket
-function sendMessage($conn, $code, $msg = "") {
-    global $replyCodes;
-    fwrite($conn, sprintf((isset($replyCodes[$code]) ? $replyCodes[$code] : "%s"), $msg) . PHP_CRLF);
+function replyToClient($conn,$msg) {
+    phpwrite('>'.$line);
+    fwrite($conn, $msg . PHP_CRLF);
 }
 
 function phpwrite($message, $socketid = 0, $priority = LOG_ALERT) {
